fb6789e60330b2319efaa9f5d8499c7688db4ad3
kauan777 <kauan7622@gmail.com>
2023-04-26 18:04:57 -0300

    feat: including chat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import ScrollToTop from "react-scroll-to-top";
+//@ts-ignore
+import TawkTo from "tawkto-react";
 import Navbar from "../Components/Navbar";
 import Slide from "../Components/Slide";
 import Page from "../Components/Page";
@@ -11,15 +13,32 @@ import Featured from "../Components/Featured";
 import Footer from "../Components/Footer";
 import BoxSponsers from "../Components/BoxSponsers";
 import ModalForm from "../Components/ModalForm";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    var tawk = new TawkTo(process.env.NEXT_PUBLIC_PROPERTY_ID, process.env.NEXT_PUBLIC_TAWK_ID);
+
+    tawk.onStatusChange((status: any) => {
+      console.log(status);
+    });
+  }, []);
+
   return (
     <Page
       title="Casa4u - Móveis Planejados"
       description="Casa4u fornece aos clientes a melhor experiência de ambiente possível, construindo sonhos, seja ele qual for. Cozinha planejada, Lavabos, Quartos planejados, Office e muito mais."
       path="/"
     >
-      <ScrollToTop smooth color="#14213d" style={{ borderRadius: 100, padding: "0rem 0rem 0rem .4rem", zIndex: 20 }} />
+      <ScrollToTop
+        smooth
+        color="#14213d"
+        style={{
+          borderRadius: 100,
+          padding: "0rem 0rem 0rem .4rem",
+          zIndex: 20,
+        }}
+      />
       <Navbar />
       <main className="contextModal">
         <Slide />
